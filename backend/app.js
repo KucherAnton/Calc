92dcf801babe402f663a56d464fb1d57d89c8257
Антон Кучер <kapetrovi4@gmail.com>
2022-09-02 11:37:01 +0300

    variables and clear all function

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,19 @@
-let penis = 0.2 + 3.5;
+const output = document.querySelector('.resultScreen p');
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['+', '-', '*', '/'];
 
-console.log(penis);
+let firstNum = '';
+let secondNum = '';
+let sign = '';
+let result = false;
+
+document.querySelector('.eraseAll').addEventListener('click', clearAll);
+document.querySelector('.buttons').addEventListener('click', pressedButton);
+
+function clearAll() {
+	firstNum = '';
+	secondNum = '';
+	sign = '';
+	result = false;
+	output.textContent = 0;
+}
