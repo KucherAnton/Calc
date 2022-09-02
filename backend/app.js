435be1f4a18e63c7c94771b91443637dbc665b33
Антон Кучер <kapetrovi4@gmail.com>
2022-09-02 12:43:31 +0300

    main math functions

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const output = document.querySelector('.resultScreen p');
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const action = ['+', '-', '*', '/'];
+const action = ['+', '-', '*', '/', '%'];
 
 let firstNum = '';
 let secondNum = '';
 let sign = '';
-let result = false;
+let equal = false;
 
 document.querySelector('.eraseAll').addEventListener('click', clearAll);
 document.querySelector('.buttons').addEventListener('click', pressedButton);
@@ -16,4 +16,55 @@ function clearAll() {
 	sign = '';
 	result = false;
 	output.textContent = 0;
+}
+
+function pressedButton(event) {
+	const key = event.target.textContent;
+
+	output.textContent = '';
+
+	if (!event.target.classList.contains('btn')) return;
+	if (event.target.classList.contains('eraseAll')) return;
+
+	if (digit.includes(key)) {
+		if (secondNum == '' && sign == '') {
+			firstNum += key;
+			output.textContent = firstNum;
+		} else if (firstNum != '' && secondNum != '' && equal) {
+			secondNum = key;
+			equal = false;
+			output.textContent = secondNum;
+		} else {
+			secondNum += key;
+			output.textContent = secondNum;
+		}
+		return;
+	}
+
+	if (action.includes(key)) {
+		sign = key;
+		output.textContent = key;
+		console.log(key);
+		return;
+	}
+
+	if (key == '=') {
+		switch (sign) {
+			case '+':
+				firstNum = +firstNum + +secondNum;
+				break;
+			case '-':
+				firstNum = firstNum - secondNum;
+				break;
+			case '*':
+				firstNum = firstNum * secondNum;
+				break;
+			case '/':
+				firstNum = firstNum / secondNum;
+				break;
+			case '%':
+		}
+		equal = true;
+		output.textContent = firstNum;
+	}
 }
